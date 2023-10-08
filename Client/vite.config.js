78fd7417460dd48bfcdf35d766722afd6b6227c5
natnail getachew <natnailgetachew6@gmail.com>
2023-10-08 13:50:58 +0300

    thired  deployment

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+      vue(),
+      laravel({
+          input: ['resources/js/app.js'],
+          refresh: true,
+      }),
+      i18n(),
+  ],
+
+  resolve: {
+      alias: {
+          vue: 'vue/dist/vue.esm-bundler.js',
+          ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
+
+      },
+  },
+  build: {
+      rollupOptions: {
+          output:{
+              manualChunks(id) {
+                  if (id.includes('node_modules')) {
+                      return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                  }
+              }
+          }
+      }
+  }
+});
